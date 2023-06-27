4e50c8ea2dfd663fc0109278a0bf5283da48ee6f
Fernando Morales Ralda <fernando.morales@ubiquo.io>
2023-06-26 18:26:04 -0600

    Adding info to main page

--- proyecto2-frontend/src/components/blog/Blog.js
+++ proyecto2-frontend/src/components/blog/Blog.js
@@ -16,42 +16,28 @@ import post1 from './blog-post.1.md';
 import post2 from './blog-post.2.md';
 import post3 from './blog-post.3.md';
 
-const sections = [
-  { title: 'Technology', url: '#' },
-  { title: 'Design', url: '#' },
-  { title: 'Culture', url: '#' },
-  { title: 'Business', url: '#' },
-  { title: 'Politics', url: '#' },
-  { title: 'Opinion', url: '#' },
-  { title: 'Science', url: '#' },
-  { title: 'Health', url: '#' },
-  { title: 'Style', url: '#' },
-  { title: 'Travel', url: '#' },
-];
+const sections = [];
 
 const mainFeaturedPost = {
-  title: 'Title of a longer featured blog post',
+  title: 'Pelis',
   description:
-    "Multiple lines of text that form the lede, informing new readers quickly and efficiently about what's most interesting in this post's contents.",
+    "Mira películas sin limites ni restricciones",
   image: 'https://source.unsplash.com/random?wallpapers',
   imageText: 'main image description',
-  linkText: 'Continue reading…',
 };
 
 const featuredPosts = [
   {
-    title: 'Featured post',
-    date: 'Nov 12',
+    title: 'Crea una playlist',
     description:
-      'This is a wider card with supporting text below as a natural lead-in to additional content.',
+      'Busca películas que deseas y agrégalas para ver cuando quieras',
     image: 'https://source.unsplash.com/random?wallpapers',
     imageLabel: 'Image Text',
   },
   {
-    title: 'Post title',
-    date: 'Nov 11',
+    title: 'Comenta las películas',
     description:
-      'This is a wider card with supporting text below as a natural lead-in to additional content.',
+      'publica lo que opinas de las películas luego de verlas',
     image: 'https://source.unsplash.com/random?wallpapers',
     imageLabel: 'Image Text',
   },
@@ -91,7 +77,7 @@ export default function Blog() {
     <ThemeProvider theme={defaultTheme}>
       <CssBaseline />
       <Container maxWidth="lg">
-        <Header title="Blog" sections={sections} />
+        <Header title="Películas" sections={sections} />
         <main>
           <MainFeaturedPost post={mainFeaturedPost} />
           <Grid container spacing={4}>
@@ -99,20 +85,11 @@ export default function Blog() {
               <FeaturedPost key={post.title} post={post} />
             ))}
           </Grid>
-          <Grid container spacing={5} sx={{ mt: 3 }}>
-            <Main title="From the firehose" posts={posts} />
-            <Sidebar
-              title={sidebar.title}
-              description={sidebar.description}
-              archives={sidebar.archives}
-              social={sidebar.social}
-            />
-          </Grid>
         </main>
       </Container>
       <Footer
-        title="Footer"
-        description="Something here to give the footer a purpose!"
+        title="Practica 2 IPC1"
+        description="Fernando Morales 202003654"
       />
     </ThemeProvider>
   );
